09b713470c8f95f633f05300214b10463468c65a
David19 <davidleonel911@gmail.com>
2025-11-17 20:42:29 -0500

    .

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.shortcuts import render, redirect 
+
+from django.http import HttpResponse
+
 # Create your views here.
+
+def prueba(request):
+    #return  HttpResponse("working")
+    return render(request, 'a.html')
